6328ed9cdf0bcb46b534ee1d10cd7353d580c12f
Mangesh <mangesh.kadam@citruspay.com>
2015-04-24 19:51:22 +0530

    * GA events enums.

--- citruslibrary/src/com/citrus/analytics/ConnectionType.java
+++ citruslibrary/src/com/citrus/analytics/ConnectionType.java
@@ -4,10 +4,20 @@ package com.citrus.analytics;
  * Created by MANGESH KADAM on 4/21/2015.
  */
 public enum ConnectionType {
-    WIFI,
-    MOBILE_2G,
-    MOBILE_3G,
-    MOBILE_4G,
-    NOT_CONNECTED,
-    UNKNOWN
+    MOBILE_2G(23),
+    MOBILE_3G(29),
+    MOBILE_4G(31),
+    WIFI(37),
+    UNKNOWN(41),
+    NOT_CONNECTED(43);
+
+    private final int connectionType;
+
+    ConnectionType(int connectionType) {
+        this.connectionType = connectionType;
+    }
+
+    public int getValue() {
+        return  connectionType;
+    }
 }
